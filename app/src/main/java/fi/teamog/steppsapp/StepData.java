3c54d50949a37ad89e0c879c35f8d91c204b3d9a
4MBL <74970679+4MBL@users.noreply.github.com>
2022-09-30 08:20:31 +0300

    created StepData singleton

---
+++ app/src/main/java/fi/teamog/steppsapp/StepData.java
@@ -0,0 +1,36 @@
+package fi.teamog.steppsapp;
+
+import java.util.HashMap;
+
+/**
+ * Singleton that stores data in Day objects in a HashMap
+ * @author Leo Härkönen
+ */
+public class StepData {
+    private static final StepData ourInstance = new StepData();
+    private static HashMap<String, Day> days = new HashMap<>();
+
+    public static StepData getInstance() {
+        return ourInstance;
+    }
+
+    private StepData() {
+    }
+
+    /**
+     * Adds a day to the HashMap with ISO date as the key and Day object as the value.
+     * @param dateIso date in ISO format string, for example "2007-07-17"
+     */
+    public void addDay(String dateIso) {
+        days.put(dateIso, new Day());
+    }
+
+    /**
+     * Gets a day by ISO date string.
+     * @param dateIso date in ISO format string, for example "2007-07-17"
+     * @return a Day object
+     */
+    public Day getDay(String dateIso) {
+        return days.get(dateIso);
+    }
+}
