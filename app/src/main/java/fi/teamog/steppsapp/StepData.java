c978ab79cca2c8051fb8cc4eedb5a91a2bfdcc6d
4MBL <74970679+4MBL@users.noreply.github.com>
2022-10-03 09:01:32 +0300

    added javadoc to getInstance method

--- app/src/main/java/fi/teamog/steppsapp/StepData.java
+++ app/src/main/java/fi/teamog/steppsapp/StepData.java
@@ -10,6 +10,10 @@ public class StepData {
     private static final StepData ourInstance = new StepData();
     private static HashMap<String, Day> days = new HashMap<>();
 
+    /**
+     * Access to singleton instance of StepData.
+     * @return singleton instance of StepData
+     */
     public static StepData getInstance() {
         return ourInstance;
     }
@@ -28,9 +32,9 @@ public class StepData {
     /**
      * Gets a day by ISO date string.
      * @param dateIso date in ISO format string, for example "2007-07-17"
-     * @return a Day object
+     * @return a Day object or null if object does not exist in the HashMap
      */
     public Day getDay(String dateIso) {
-        return days.get(dateIso);
+        return days.getOrDefault(dateIso, null);
     }
 }
