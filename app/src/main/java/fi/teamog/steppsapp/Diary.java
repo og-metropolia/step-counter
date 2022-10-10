1c24cfdec36be8d5ed13199f72b1bb5d454dedb9
gashi <gashiad@outlook.com>
2022-10-10 12:44:22 +0300

    created methods in Diary

--- app/src/main/java/fi/teamog/steppsapp/Diary.java
+++ app/src/main/java/fi/teamog/steppsapp/Diary.java
@@ -5,24 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Diary extends AppCompatActivity {
+
+    String mood;
+    EditText moodNow;
+    ArrayList moodList;
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        View saveButton = findViewById(R.id.buttonSaveDiary);
+        moodNow = findViewById(R.id.moodTextMultiLine);
+
+        moodList = new ArrayList<>();
+
+        lv = findViewById(R.id.listViewMoods);
+
+        lv.setAdapter(new ArrayAdapter<String>(
+                this,  //context (activity instance)
+                R.layout.mood_item, //XML item layout
+                moodList));
+    }
+
+    public void onClick(View view) {
+        if(view.getId() == R.id.buttonSaveDiary) {
+            mood = moodNow.getText().toString();
+            moodList.add(mood);
+            ((ArrayAdapter) lv.getAdapter()).notifyDataSetChanged();
+            moodNow.setText("");
+            Log.e("Test", "Nappi toimii");
+
+        }
 
     }
-    private void saveData(String value) {
-        SharedPreferences prefPut = getSharedPreferences("logData", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor prefEditor = prefPut.edit();
-        prefEditor.putString(getString(R.string.diary_preference_key), value);
-        prefEditor.commit();
-    }
-
 }
