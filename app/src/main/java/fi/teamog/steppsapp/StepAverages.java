2197b7f9d8d7e5411058c74c6fb7e56c38f8c9d1
TaaviNat <taavi.natynki@gmail.com>
2022-10-10 12:09:10 +0300

    Raportista liikutettiin logiikka omaan luokkaan.

---
+++ app/src/main/java/fi/teamog/steppsapp/StepAverages.java
@@ -0,0 +1,21 @@
+package fi.teamog.steppsapp;
+
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class StepAverages {
+
+    public int getThreeDayAverage() {
+        String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String threeDaysAgo = LocalDate.now().minusDays(3).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        return StepData.getInstance().getPeriodSteps(threeDaysAgo, today) / 3;
+    }
+
+    public int getThirtyDayAverage() {
+        String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String thirtyDaysAgo = LocalDate.now().minusDays(30).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        return StepData.getInstance().getPeriodSteps(thirtyDaysAgo, today) / 30;
+    }
+}
