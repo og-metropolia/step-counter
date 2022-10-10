4d0024f9d6494511a1bcd08c04c9f1b65d07d4f2
4MBL <74970679+4MBL@users.noreply.github.com>
2022-10-10 12:40:30 +0300

    added javadoc comments

--- app/src/main/java/fi/teamog/steppsapp/StepAverages.java
+++ app/src/main/java/fi/teamog/steppsapp/StepAverages.java
@@ -14,12 +14,21 @@ public class StepAverages {
 
     private StepAverages() {
     }
+
+    /**
+     * Gets steps within the last 3 days.
+     * @return amount of steps within the last 3 days
+     */
     public int getThreeDayAverage() {
         String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
         String threeDaysAgo = LocalDate.now().minusDays(3).format(DateTimeFormatter.ISO_LOCAL_DATE);
         return StepData.getInstance().getPeriodSteps(threeDaysAgo, today) / 3;
     }
 
+    /**
+     * Gets steps within the last 30 days.
+     * @return amount of steps within the last 30 days
+     */
     public int getThirtyDayAverage() {
         String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
         String thirtyDaysAgo = LocalDate.now().minusDays(30).format(DateTimeFormatter.ISO_LOCAL_DATE);
