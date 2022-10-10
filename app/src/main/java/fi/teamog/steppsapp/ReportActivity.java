42bdfc399928ae93bece775dbb54c8d46c0356f1
TaaviNat <taavi.natynki@gmail.com>
2022-10-10 11:50:27 +0300

    Raportti-sivu tehty

--- app/src/main/java/fi/teamog/steppsapp/ReportActivity.java
+++ app/src/main/java/fi/teamog/steppsapp/ReportActivity.java
@@ -3,8 +3,10 @@ package fi.teamog.steppsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -14,10 +16,35 @@ public class ReportActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
+        StepData.getInstance().addDay("2022-09-29");
+        Day day = StepData.getInstance().getDay("2022-09-29");
+        day.addSteps(6,3000);
+        day.addSteps(8,2500);
+        StepData.getInstance().saveData(this);
+        StepData.getInstance().loadPreviousData(this);
 
-        StepData stepData = StepData.getInstance();
-        LocalDate today = LocalDate.now();
-        LocalDate threeDaysAgo = LocalDate.now().minusDays(3);
-        StepData.getInstance().getPeriodSteps(StepData.getInstance().getIsoDateFormat().format(threeDaysAgo), StepData.getInstance().getIsoDateFormat().format(today));
+        StepData.getInstance().addDay("2022-10-09");
+        Day day2 = StepData.getInstance().getDay("2022-10-09");
+        day.addSteps(6,1000);
+        day.addSteps(8,2500);
+        StepData.getInstance().saveData(this);
+        StepData.getInstance().loadPreviousData(this);
+
+        StepData.getInstance().addDay("2022-10-10");
+        Day day3 = StepData.getInstance().getDay("2022-10-10");
+        day.addSteps(6,1000);
+        day.addSteps(8,2500);
+        StepData.getInstance().saveData(this);
+        StepData.getInstance().loadPreviousData(this);
+
+        String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String threeDaysAgo = LocalDate.now().minusDays(3).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        String thirtyDaysAgo = LocalDate.now().minusDays(30).format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        TextView tvThreeDayAvg = findViewById(R.id.TextViewThirtyDaysValue);
+        tvThreeDayAvg.setText(String.valueOf(StepData.getInstance().getPeriodSteps(threeDaysAgo, today)/3));
+
+        TextView tvThirtyDayAvg = findViewById(R.id.TextViewLastThreeDaysValue);
+        tvThirtyDayAvg.setText(String.valueOf(StepData.getInstance().getPeriodSteps(thirtyDaysAgo, today)/30));
     }
 }
